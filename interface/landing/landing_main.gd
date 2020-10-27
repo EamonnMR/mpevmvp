ffe70ded3d5042d83dde7ba362e5885f76b4f3e0
EamonnMR <Eamonn.m.r@gmail.com>
2020-10-27 09:11:59 -0400

    Hide unused landing buttons

--- interface/landing/landing_main.gd
+++ interface/landing/landing_main.gd
@@ -5,11 +5,20 @@ var spob: Node
 func set_spob(new_spob: Node):
 	spob = new_spob
 	$Panel/Label.text = new_spob.name
+	$Panel/Info.text = """
+Type: %s %s
+Faction: %s
+	""" % [
+		Game.spob_types[new_spob.spob_type]["biome"],
+		Game.spob_types[new_spob.spob_type]["kind"],
+		Game.factions[new_spob.faction]["name"] if new_spob.faction else "uninhabited"
+	]
 	$Panel/Picture.texture = new_spob.landing
 	$Exchange.setup_for(spob)
 	# TODO: Refresh store with available ships
 	# TODO: Refessh outfitter with available outfits
-	# TODO: Refresh trade center with prices
+	$Panel/TradeButton.show() if new_spob.commodities.size() else $Panel/TradeButton.hide()
+	$Panel/ShipyardButton.show() if new_spob.faction else $Panel/ShipyardButton.hide()
 
 func _ready():
 	$Panel/LeaveButton.connect("pressed", Client.player_input, "toggle_landing")
